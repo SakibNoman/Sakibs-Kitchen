22e7250fdc82c416e7e99d5d5e5da756157b2257
Sakib Noman <skbnmn@gmail.com>
2021-02-07 14:09:24 +0600

    details view added

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const userInput = document.getElementById("search-box");
 document.getElementById("search-btn").addEventListener("click", search => {
     searchApi(userInput.value);
 })
-const searchApi = searchMeal =>{
+const searchApi = searchMeal => {
 
     let api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`
     fetch(api)
-    .then(res => res.json())
-    .then(data => {
-        makeCard(data.meals);
-    })
+        .then(res => res.json())
+        .then(data => {
+            makeCard(data.meals);
+        })
 }
 
-
 const allCards = document.getElementById("all-cards");
-const makeCard = content =>{
+const makeCard = content => {
     allCards.innerHTML = "";
     content.forEach(element => {
         const singleCard = `
-            <div class="card border-0" style="width: 14rem; height: 16rem;">
+            <div onclick = "detailsView('${element.idMeal}')" class="card border-0" style="width: 14rem; height: 16rem;">
                 <img src="${element.strMealThumb}" class="card-img-top h-75 w-100" alt="...">
                 <div class="card-body overflow-auto ">
                     <p class="card-text text-center fw-bold">${element.strMeal}</p>
@@ -28,5 +27,32 @@ const makeCard = content =>{
             </div>
         `;
         allCards.innerHTML += singleCard;
-    }); 
+    });
+}
+
+const detailsView = mealId => {
+    const detailsParent = document.getElementById("details-card");
+    let api = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            const detailsCard = `
+        <div class="card border-0" style="width: 26rem;">
+            <img class="card-img-top" src="${data.meals[0].strMealThumb}" >
+                <div class="card-body">
+                    <h1 class="card-text fs-3 fw-bold" >${data.meals[0].strMeal}</h1>
+                    <p class="card-text fw-bold">Ingredients</p>
+                    <ul>
+                        <li>${data.meals[0].strIngredient1}</li>
+                    </ul>
+                </div>
+        </div> 
+        `;
+        detailsParent.innerHTML = detailsCard;
+        })
+
+}
+let n=1;
+while("strIngredient"+n != null){
+    
 }
